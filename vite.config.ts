aa499033598ea7d15cc1616318c28a03f824d097
Sanjana Deshmukh <sanjana060504@gmail.com>
2026-09-23 18:50:44 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Sanjana-s-Portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
